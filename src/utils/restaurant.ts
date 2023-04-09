6b36fe95566de9f69c0047fce148093fc597f5fe
Aamir Khan <aamir.fastnu@gmail.com>
2023-04-09 16:58:56 +0500

    Fix inverse is closed conditional

--- src/utils/restaurant.ts
+++ src/utils/restaurant.ts
@@ -5,5 +5,5 @@ import { getDayOfTheWeek } from './time';
 export const isClosedToday = (openingHours: OpeningHours): boolean => {
   const today = getDayOfTheWeek(new Date().getDay());
   const hoursToday = openingHours[today];
-  return Boolean(hoursToday.length);
+  return Boolean(!hoursToday.length);
 };
